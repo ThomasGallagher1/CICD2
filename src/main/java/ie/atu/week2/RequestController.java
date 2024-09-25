5fce20db5b822d7d6f5b53825c564eddd291ab13
G00415457@atu.ie <G00415457@atu.ie>
2024-09-25 15:02:55 +0100

    added greetbyName and details methods

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,14 +1,21 @@
 package ie.atu.week2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Person")
 public class RequestController {
-    @GetMapping("greet/{name}")
+    @GetMapping("/hello")
     public String sayHello(){
         return("Hi There :");
+    }
+    @GetMapping("greet/{name}")
+    public String greetByName(@PathVariable() String name){
+        return("Your name is: " + name);
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam () String name, @RequestParam() int age){
+        return  name + " " + age;
     }
 }
