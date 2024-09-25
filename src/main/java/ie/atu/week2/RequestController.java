b7b9ee917c6e9d7540f482f6d56b354c31b497bb
G00415457@atu.ie <G00415457@atu.ie>
2024-09-25 15:58:32 +0100

    Week 2 assignment

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -3,7 +3,6 @@ package ie.atu.week2;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Person")
 public class RequestController {
     @GetMapping("/hello")
     public String sayHello(){
@@ -18,5 +17,31 @@ public class RequestController {
     public String details(@RequestParam () String name, @RequestParam() int age){
         return  name + " " + age;
     }
-
+    @GetMapping("calculate")
+    public String calculate(@RequestParam () int num1, @RequestParam() int num2, @RequestParam () String operation){
+       float total = 0;
+        switch(operation){
+            case "add":
+                total = num1 + num2;
+                break;
+            case "subtract":
+                total = num1 - num2;
+                break;
+            case "multiply":
+                total = num1 * num2;
+                break;
+            case "divide":
+                if(num2 == 0){
+                    System.out.print("Cant divide by 0");
+                    break;
+                }else{
+                    total = num1 / num2;
+                }
+                break;
+            default:
+                System.out.print("invalid input");
+                break;
+        }
+        return Float.toString(total);
+    }
 }
